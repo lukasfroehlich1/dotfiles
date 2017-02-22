4b69d33687349db4d28adf026210e8a4e6c426c0
Lukas Froehlich <lukasfroehlich1@gmail.com>
2017-02-21 18:20:58 -0800

    addded installing of all plugins

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # Get current dir (so run this script from anywhere)
-
 export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -22,5 +21,8 @@ ln -sfv "$DOTFILES_DIR/vim/monokai.vim" ~/.vim/colors
 ln -sfv "$DOTFILES_DIR/vim/ftplugin/" ~/.vim/ftplugin
 ln -sfv "$DOTFILES_DIR/vim/runcom/" ~/.vim/runcom
 
-# installing vundle
+# installing vundle for vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+# install all listed plugins
+vim -c VundleUpdate -c quitall
